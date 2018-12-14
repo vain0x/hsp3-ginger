b4a50e07392bf771b9f192afeb74ea7ea1647381
vain0x <vainzerox@gmail.com>
2018-12-14 23:36:27 +0900

    chore: ビルドスクリプトを追加

---
+++ scripts/build.ps1
@@ -0,0 +1,33 @@
+cargo --version
+if (!$?) { exit 1}
+
+npm --version
+if (!$?) { exit 1 }
+
+vsce --version
+if (!$?) {
+    npm instal -g vsce
+    if (!$?) { exit 1 }
+}
+
+cd hsp3debug
+msbuild /p:Configuration=Release /p:Platform=x86
+cd ..
+cp ./hsp3debug/Release/hsp3-debug-ginger.dll ./vscode-ext/out/x86-sjis/hsp3debug.dll
+
+cd middle-adapter
+cargo build --release
+cd ..
+cp ./middle-adapter/target/release/middle-adapter.exe ./vscode-ext/out/middle-adapter.exe
+
+cd adapter
+cargo build --release --target i686-pc-windows-msvc
+cd ..
+cp ./adapter/target/i686-pc-windows-msvc/release/hsp3_debug_adapter.dll ./vscode-ext/out/x86-sjis/hsp3debug-ginger-adapter.dll
+
+cd vscode-ext
+npm run compile
+if (!$?) { exit 1 }
+vsce package -o build.vsix
+if (!$?) { exit 1 }
+cd ..
