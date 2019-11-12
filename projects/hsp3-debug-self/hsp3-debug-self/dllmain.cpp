d50b03b290bc56d06f86893930d1e80c778a053b
vain0x <vainzerox@gmail.com>
2019-11-13 07:55:49 +0900

    feat: logmes に対応

--- projects/hsp3-debug-self/hsp3-debug-self/dllmain.cpp
+++ projects/hsp3-debug-self/hsp3-debug-self/dllmain.cpp
@@ -12,6 +12,7 @@
 
 #define WM_HDS_HELLO      (WM_USER +  0)
 #define WM_HDS_SHUTDOWN   (WM_USER +  1)
+#define WM_HDS_LOGMES     (WM_USER +  2)
 
 // assert, stop やステップ実行の完了により、HSP スクリプトの実行が一時停止したとき。
 static auto const HSP3DEBUG_NOTICE_STOP = 0;
@@ -24,11 +25,16 @@ static auto const MAX_DATA_SIZE = 0x10000;
 using OsString = std::basic_string<TCHAR>;
 using OsStringView = std::basic_string_view<TCHAR>;
 
+static auto s_instance = std::optional<HINSTANCE>{};
+static auto s_debug = std::optional<HSP3DEBUG*>{};
+static auto s_hds_client_hwnd = std::optional<HWND>{};
 static auto s_hds_client_process = std::optional<HANDLE>{};
 static auto s_hds_client_thread = std::optional<HANDLE>{};
-static auto s_hds_client_hwnd = std::optional<HWND>{};
 static auto s_hds_pipe_name = OsString{ TEXT("\\\\.\\pipe\\hdspipe") };
 static auto s_hds_pipe_server = std::optional<std::thread>{};
+static auto s_hds_down_buffer_name = OsString{ TEXT("hdsdownbuffer") };
+static auto s_hds_down_buffer_handle = std::optional<HANDLE>{};
+static auto s_hds_down_buffer_view = std::optional<char*>{};
 static auto s_terminated = false;
 
 static auto get_hsp_dir() -> OsString {
@@ -49,6 +55,12 @@ static auto get_hsp_dir() -> OsString {
 
 	return full_path;
 }
+
+// -----------------------------------------------
+// パイプ
+//
+// クライアントにメッセージを送信する。
+// -----------------------------------------------
 
 static auto serve_hds_pipe() -> int {
 	static auto const MAX_INSTANCE_COUNT = 2;
@@ -160,9 +172,112 @@ static auto serve_hds_pipe() -> int {
 	return 0;
 }
 
-static void app_initialize() {
+// -----------------------------------------------
+// メモリマップトファイル
+//
+// クライアントにメッセージの内容を渡すための領域。
+// -----------------------------------------------
+
+static auto create_down_buffer() -> bool {
+	static auto BUFFER_SIZE = 0x8000;
+	auto handle = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		BUFFER_SIZE,
+		s_hds_down_buffer_name.data()
+	);
+	if (!handle) {
+		OutputDebugString(TEXT("could not create buffer"));
+		return false;
+	}
+	s_hds_down_buffer_handle = handle;
+
+	auto view = MapViewOfFile(handle, FILE_MAP_WRITE, 0, 0, 0);
+	if (!view) {
+		OutputDebugString(TEXT("Couldn't map buffer"));
+		return false;
+	}
+	s_hds_down_buffer_view = reinterpret_cast<char*>(view);
+
+	return true;
+}
+
+// -----------------------------------------------
+// メインウィンドウ
+// -----------------------------------------------
+
+static LRESULT WINAPI main_window_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+	//case WM_USER:
+	//	OutputDebugString(TEXT("dbg_set"));
+	//	(*s_debug)->dbg_set(HSPDEBUG_RUN);
+	//	PostMessage(HWND_BROADCAST, WM_NULL, WPARAM{}, LPARAM{});
+	//	break;
+
+	case WM_CREATE:
+		return TRUE;
+
+	case WM_CLOSE:
+		return FALSE;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+	return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+static auto create_main_window() -> HWND {
+	static auto const WINDOW_CLASS_NAME = TEXT("HdsMainWindowClass");
+
+	auto wndclass = WNDCLASS{};
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = main_window_proc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = *s_instance;
+	wndclass.hIcon = nullptr;
+	wndclass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1);
+	wndclass.lpszMenuName = nullptr;
+	wndclass.lpszClassName = WINDOW_CLASS_NAME;
+	RegisterClass(&wndclass);
+
+	auto title = TEXT("hsp3-debug-self");
+	auto style = WS_CAPTION | WS_VISIBLE | WS_THICKFRAME;
+	auto size_x = 100;
+	auto size_y = 100;
+	auto pos_x = -size_x;
+	auto pos_y = -size_y;
+	auto hwnd = CreateWindow(WINDOW_CLASS_NAME, title, style, pos_x, pos_y, size_x, size_y, HWND{}, HMENU{}, *s_instance, LPARAM{});
+	if (!hwnd) {
+		MessageBox(HWND{}, TEXT("デバッグウィンドウの初期化に失敗しました。"), TEXT("hsp3-debug-self"), 0);
+		exit(EXIT_FAILURE);
+	}
+
+	ShowWindow(hwnd, SW_HIDE);
+	return hwnd;
+}
+
+// -----------------------------------------------
+// アプリケーション
+// -----------------------------------------------
+
+static void app_initialize(HSP3DEBUG* debug) {
+	s_debug = debug;
+
 	s_hds_pipe_server = std::make_optional(std::thread{ [] { serve_hds_pipe(); } });
 
+	if (!create_down_buffer()) {
+		return;
+	}
+
+	// start client:
 	auto name = get_hsp_dir();
 	name += TEXT("hds_client_proxy.exe");
 
@@ -203,6 +318,26 @@ static void app_terminate() {
 	}
 }
 
+static void app_logmes(char const* text) {
+	// FIXME: hello までの logmes はバッファーに溜めておき、hello の時点で送信する
+
+	if (s_terminated || !s_hds_client_hwnd || !s_hds_down_buffer_view) {
+		return;
+	}
+	auto buffer = *s_hds_down_buffer_view;
+
+	auto size = std::strlen(text);
+	memcpy(buffer, text, size);
+	buffer[size] = '\0';
+
+	SendMessage(*s_hds_client_hwnd, WM_HDS_LOGMES, size, LPARAM{});
+	return;
+}
+
+// -----------------------------------------------
+// エントリーポイント
+// -----------------------------------------------
+
 BOOL APIENTRY DllMain(HMODULE instance, DWORD reason, LPVOID _reserved) {
 	switch (reason) {
 	case DLL_PROCESS_ATTACH:
@@ -218,10 +353,13 @@ BOOL APIENTRY DllMain(HMODULE instance, DWORD reason, LPVOID _reserved) {
 		}
 #endif
 		OutputDebugString(TEXT("hsp3debug attach\n"));
+		s_instance = instance;
 		break;
+
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 		break;
+
 	case DLL_PROCESS_DETACH:
 		OutputDebugString(TEXT("hsp3debug detach\n"));
 		app_terminate();
@@ -234,7 +372,7 @@ BOOL APIENTRY DllMain(HMODULE instance, DWORD reason, LPVOID _reserved) {
 EXPORT BOOL APIENTRY debugini(HSP3DEBUG* debug, int _nouse1, int _nouse2, int _nouse3) {
 	OutputDebugString(TEXT("debugini\n"));
 
-	app_initialize();
+	app_initialize(debug);
 	return 0;
 }
 
@@ -249,6 +387,8 @@ EXPORT BOOL WINAPI debug_notice(HSP3DEBUG* debug, int reason, int _nouse1, int _
 		OutputDebugString(TEXT("debug_notice (logmes): {\"\n"));
 		OutputDebugStringA(debug->hspctx->stmp); // FIXME: 文字コード変換
 		OutputDebugString(TEXT("\n\"}\n"));
+
+		app_logmes(debug->hspctx->stmp);
 		break;
 
 	default:
