94065b3dc5309601bea05fa946e527a086ece4bb
vain0x <vainzerox@gmail.com>
2020-02-03 21:48:29 +0900

    feat: GreenElement に From を実装させる

--- hsp3-forgery/hf_core/src/syntax/green_element.rs
+++ hsp3-forgery/hf_core/src/syntax/green_element.rs
@@ -6,6 +6,18 @@ pub(crate) enum GreenElement {
     Node(GreenNode),
 }
 
+impl From<TokenData> for GreenElement {
+    fn from(token: TokenData) -> Self {
+        GreenElement::Token(token)
+    }
+}
+
+impl From<GreenNode> for GreenElement {
+    fn from(node: GreenNode) -> Self {
+        GreenElement::Node(node)
+    }
+}
+
 impl fmt::Debug for GreenElement {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
