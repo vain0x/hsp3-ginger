cc3c2f1c3a17ea62a7e023b67d19a2f4bd3c5160
vain0x <vainzerox@gmail.com>
2020-02-04 00:14:39 +0900

    feat: child_elements が SyntaxElement を生成するようにする

--- hsp3-forgery/hf_core/src/syntax/syntax_node.rs
+++ hsp3-forgery/hf_core/src/syntax/syntax_node.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::fmt;
 use std::rc::Rc;
 
 pub(crate) struct SyntaxNode {
@@ -34,45 +35,49 @@ impl SyntaxNode {
         }
     }
 
-    pub(crate) fn child_elements(&self) -> impl Iterator<Item = &GreenElement> {
-        self.green().children().iter()
-    }
-
-    pub(crate) fn child_nodes(self: Rc<Self>) -> impl Iterator<Item = Rc<SyntaxNode>> {
+    pub(crate) fn child_elements(self: Rc<Self>) -> impl Iterator<Item = SyntaxElement> {
         // この move は self の所有権をクロージャに渡す。
         (0..self.green().children().len()).filter_map(move |child_index| {
             // self → SyntaxRoot → GreenNode (Root) → self.green() のように、
             // 間接的にイミュータブルな参照を握っているので child_index が無効になることはない。
             // そのため unwrap は失敗しないはず。
             match self.green().children().get(child_index).unwrap() {
-                GreenElement::Node(node) => Some(Rc::new(SyntaxNode {
-                    kind: node.kind(),
-                    parent: SyntaxParent::NonRoot {
-                        node: Rc::clone(&self),
-                        child_index,
-                    },
-                    // FIXME: オフセットを計算する。
-                    offset: 0,
-                })),
-                GreenElement::Token(..) => None,
+                GreenElement::Node(node) => Some(
+                    (SyntaxElement::Node(SyntaxNode {
+                        kind: node.kind(),
+                        parent: SyntaxParent::NonRoot {
+                            node: Rc::clone(&self),
+                            child_index,
+                        },
+                        // FIXME: オフセットを計算する。
+                        offset: 0,
+                    })),
+                ),
+                GreenElement::Token(token) => Some(
+                    (SyntaxElement::Token(SyntaxToken {
+                        kind: token.token(),
+                        parent: SyntaxParent::NonRoot {
+                            node: Rc::clone(&self),
+                            child_index,
+                        },
+                        location: token.location.clone(),
+                    })),
+                ),
             }
         })
     }
 
+    pub(crate) fn child_nodes(self: Rc<Self>) -> impl Iterator<Item = Rc<SyntaxNode>> {
+        self.child_elements().filter_map(|element| match element {
+            SyntaxElement::Node(node) => Some(Rc::new(node)),
+            SyntaxElement::Token(..) => None,
+        })
+    }
+
     pub(crate) fn child_tokens(self: Rc<Self>) -> impl Iterator<Item = Rc<SyntaxToken>> {
-        // 上に同じ。
-        (0..self.green().children().len()).filter_map(move |child_index| {
-            match self.green().children().get(child_index).unwrap() {
-                GreenElement::Token(token) => Some(Rc::new(SyntaxToken {
-                    kind: token.token(),
-                    parent: SyntaxParent::NonRoot {
-                        node: Rc::clone(&self),
-                        child_index,
-                    },
-                    location: token.location.clone(),
-                })),
-                GreenElement::Node(..) => None,
-            }
+        self.child_elements().filter_map(|element| match element {
+            SyntaxElement::Token(token) => Some(Rc::new(token)),
+            SyntaxElement::Node(..) => None,
         })
     }
 }
