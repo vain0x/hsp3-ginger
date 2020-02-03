1f570f5c8bd24086d88abaf1089176de3d9f0d46
vain0x <vainzerox@gmail.com>
2020-02-03 20:13:04 +0900

    feat: module/global のパース

--- hsp3-forgery/hf_core/src/parse/parse_pp.rs
+++ hsp3-forgery/hf_core/src/parse/parse_pp.rs
@@ -16,13 +16,44 @@ fn parse_end_of_pp(p: &mut Px) {
     }
 }
 
+fn parse_module_stmt_contents(p: &mut Px) {
+    assert!(p.next_data().text() == "module");
+
+    p.bump();
+
+    if !p.eat(Token::Ident) {
+        if p.next().is_str_literal_first() {
+            parse_str_literal(p);
+        }
+    }
+}
+
+fn parse_global_stmt_contents(p: &mut Px) {
+    assert!(p.next_data().text() == "global");
+
+    p.bump();
+}
+
 pub(crate) fn parse_pp_stmt(p: &mut Px) {
     assert_eq!(p.next(), Token::Hash);
 
     p.start_node();
 
     p.bump();
+
+    let kind = match p.next_data().text() {
+        "module" => {
+            parse_module_stmt_contents(p);
+            NodeKind::ModulePp
+        }
+        "global" => {
+            parse_global_stmt_contents(p);
+            NodeKind::GlobalPp
+        }
+        _ => NodeKind::UnknownPp,
+    };
+
     parse_end_of_pp(p);
 
-    p.end_node(NodeKind::UnknownPp);
+    p.end_node(kind);
 }
