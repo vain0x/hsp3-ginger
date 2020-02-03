2a616ae16076083739674a966d40491741223b1d
vain0x <vainzerox@gmail.com>
2020-02-03 19:14:05 +0900

    feat: スキップした字句を Other ノードに入れる

--- hsp3-forgery/hf_core/src/parse/parse_stmt.rs
+++ hsp3-forgery/hf_core/src/parse/parse_stmt.rs
@@ -2,22 +2,38 @@ use super::*;
 
 type Px = ParseContext;
 
-pub(crate) fn parse_root(p: &mut Px) {
-    while !p.at_eof() {
-        if p.next() == Token::Eol {
+fn parse_end_of_stmt(p: &mut Px) {
+    if !p.at_eof() && !p.next().at_end_of_stmt() {
+        p.start_node();
+
+        while !p.at_eof() && !p.next().at_end_of_stmt() {
             p.bump();
-            continue;
         }
 
+        p.end_node(NodeKind::Other);
+    }
+
+    if !p.at_eof() {
+        assert!(p.next().at_end_of_stmt());
+        p.bump();
+    }
+}
+
+pub(crate) fn parse_root(p: &mut Px) {
+    while !p.at_eof() {
         // エラー回復
-        if !p.next().is_stmt_first() {
+        if !p.next().is_stmt_first() && !p.next().at_end_of_stmt() {
+            p.start_node();
+
             while !p.at_eof() && !p.next().is_stmt_first() {
                 p.bump();
             }
+
+            p.end_node(NodeKind::Other);
             continue;
         }
 
-        // let stmt = parse_stmt(p);
-        p.bump();
+        // parse_stmt(p);
+        parse_end_of_stmt(p)
     }
 }
