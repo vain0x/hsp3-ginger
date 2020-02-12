f3fc3abbbc784179eccc649feb269e53b638b307
vain0x <vainzerox@gmail.com>
2020-02-12 21:45:25 +0900

    feat: LSP がファイルの開閉や変更を検出して構文エラーを報告するようにする

--- hsp3-forgery/hf_lsp/src/lsp_model.rs
+++ hsp3-forgery/hf_lsp/src/lsp_model.rs
@@ -1,14 +1,17 @@
 use crate::file_changes::FileChanges;
 use crate::file_watcher::FileWatcher;
+use hsp3_forgery_core::api::World;
 use lsp_types::*;
 use std::collections::HashSet;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
 
 #[derive(Default)]
 pub(crate) struct LspModel {
-    editing_files: HashSet<PathBuf>,
+    editing_files: HashSet<Rc<PathBuf>>,
     file_watcher: Option<FileWatcher>,
     file_changes: FileChanges,
+    world: World,
     hsp_root: PathBuf,
 }
 
@@ -38,6 +41,23 @@ impl LspModel {
     }
 
     fn on_file_changes(&mut self) {
+        for updated_path in &self.file_changes.updated_paths {
+            if self.editing_files.contains(updated_path)
+                || self.file_changes.removed_paths.contains(updated_path)
+            {
+                continue;
+            }
+
+            if let Some(source_code) = read_file(updated_path) {
+                let source_path = Rc::new(updated_path.to_owned());
+                self.world.set_source_code(source_path, source_code);
+            }
+        }
+
+        for removed_path in &self.file_changes.removed_paths {
+            self.world.remove_source_file(Rc::new(removed_path.clone()));
+        }
+
         if self.file_changes.disconnected {
             self.file_watcher.take();
         }
@@ -45,33 +65,39 @@ impl LspModel {
         self.file_changes.clear();
     }
 
-    pub(crate) fn open_doc(&mut self, uri: Url, _version: u64, _text: String) {
-        let file_path = match canonicalize_uri(uri) {
+    pub(crate) fn open_doc(&mut self, uri: Url, _version: u64, text: String) {
+        let source_path = match canonicalize_uri(uri) {
             None => return,
-            Some(file_path) => file_path,
+            Some(file_path) => Rc::new(file_path),
         };
 
-        self.editing_files.insert(file_path);
+        self.editing_files.insert(source_path.clone());
+        self.world.set_source_code(source_path, text);
+
         self.poll();
     }
 
-    pub(crate) fn change_doc(&mut self, uri: Url, _version: u64, _text: String) {
-        let file_path = match canonicalize_uri(uri) {
+    pub(crate) fn change_doc(&mut self, uri: Url, _version: u64, text: String) {
+        let source_path = match canonicalize_uri(uri) {
             None => return,
-            Some(file_path) => file_path,
+            Some(file_path) => Rc::new(file_path),
         };
 
-        self.editing_files.insert(file_path);
+        self.editing_files.insert(source_path.clone());
+        self.world.set_source_code(source_path, text);
+
         self.poll();
     }
 
     pub(crate) fn close_doc(&mut self, uri: Url) {
-        let file_path = match canonicalize_uri(uri) {
+        let source_path = match canonicalize_uri(uri) {
             None => return,
-            Some(file_path) => file_path,
+            Some(file_path) => Rc::new(file_path),
         };
 
-        self.editing_files.remove(&file_path);
+        self.editing_files.remove(&source_path);
+        self.world.remove_source_file(source_path);
+
         self.poll();
     }
 
@@ -103,8 +129,25 @@ impl LspModel {
         vec![]
     }
 
-    pub(crate) fn validate(&mut self, _uri: Url) -> Vec<Diagnostic> {
-        vec![]
+    pub(crate) fn validate(&mut self, uri: Url) -> Vec<Diagnostic> {
+        self.poll();
+
+        let mut diagnostics = vec![];
+
+        if let Some(source_path) = canonicalize_uri(uri) {
+            let mut forgery_diagnostics = vec![];
+            self.world
+                .get_diagnostics(Rc::new(source_path), &mut forgery_diagnostics);
+
+            diagnostics.extend(forgery_diagnostics.into_iter().map(|d| Diagnostic {
+                severity: Some(DiagnosticSeverity::Error),
+                message: d.message,
+                range: to_range(d.range),
+                ..Default::default()
+            }))
+        }
+
+        diagnostics
     }
 }
 
@@ -112,4 +155,31 @@ fn canonicalize_uri(uri: Url) -> Option<PathBuf> {
     uri.to_file_path()
         .ok()
         .and_then(|path| path.canonicalize().ok())
+}
+
+fn to_position(position: hsp3_forgery_core::api::TextPosition) -> Position {
+    Position {
+        line: position.line as u64,
+        character: position.character as u64,
+    }
+}
+
+fn to_range(range: hsp3_forgery_core::api::TextRange) -> Range {
+    Range {
+        start: to_position(range.start),
+        end: to_position(range.end),
+    }
+}
+
+fn read_file(file_path: &Path) -> Option<String> {
+    debug!("read_file {:?}", file_path);
+    let data = std::fs::read(file_path).ok()?;
+
+    let mut out = String::new();
+    if let Err(err) = crate::text_encoding::decode_as_shift_jis_or_utf8(&data, &mut out) {
+        warn!("ファイルの読込 {:?}", err);
+        return None;
+    }
+
+    Some(out)
 }
