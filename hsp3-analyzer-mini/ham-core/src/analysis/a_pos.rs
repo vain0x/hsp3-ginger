b9d8ddac5c5fc8bc4a410a809cd518ce99387d54
vain0x <vainzerox@gmail.com>
2020-06-16 18:00:04 +0900

    feat: APos::from_str, add を追加

--- hsp3-analyzer-mini/ham-core/src/analysis/a_pos.rs
+++ hsp3-analyzer-mini/ham-core/src/analysis/a_pos.rs
@@ -6,3 +6,30 @@ pub(crate) struct APos {
     /// 列番号 (0 から始まる、UTF-8 基準、バイト単位)
     pub(crate) column: usize,
 }
+
+impl APos {
+    pub(crate) fn from_str(s: &str) -> Self {
+        let mut pos = APos::default();
+
+        for c in s.chars() {
+            if c == '\n' {
+                pos.row += 1;
+                pos.column = 0;
+            } else {
+                pos.column += c.len_utf8();
+            }
+        }
+
+        pos
+    }
+
+    pub(crate) fn add(mut self, other: Self) -> Self {
+        if other.row >= 1 {
+            self.column = 0;
+        }
+
+        self.row += other.row;
+        self.column += other.column;
+        self
+    }
+}
