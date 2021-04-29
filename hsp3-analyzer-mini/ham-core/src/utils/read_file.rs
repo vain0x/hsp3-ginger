45c93b844fe20e9355e223841d14d13ba43212d3
vain0x <vainzerox@gmail.com>
2021-04-29 22:28:50 +0900

    feat: shift_jisよりUTF-8を優先する

--- hsp3-analyzer-mini/ham-core/src/utils/read_file.rs
+++ hsp3-analyzer-mini/ham-core/src/utils/read_file.rs
@@ -1,18 +1,25 @@
-use std::{fs, path::Path};
+use encoding::{DecoderTrap, Encoding};
+use std::{fs, path::Path, str};
 
-use encoding::{codec::utf_8::UTF8Encoding, DecoderTrap, Encoding, StringWriter};
-
-/// ファイルを shift_jis または UTF-8 として読む。
-pub(crate) fn read_file(file_path: &Path, out: &mut impl StringWriter) -> bool {
-    // utf-8?
-    let content = match fs::read(file_path).ok() {
+/// テキストファイルを shift_jis または UTF-8 として読む。
+pub(crate) fn read_file(file_path: &Path, out: &mut String) -> bool {
+    // バイナリで読む。
+    let contents = match fs::read(file_path).ok() {
         None => return false,
-        Some(x) => x,
+        Some(it) => it,
     };
 
-    // shift-jis?
+    // 可能ならUTF-8として読む。
+    match str::from_utf8(&contents) {
+        Ok(text) => {
+            *out += text;
+            return true;
+        }
+        Err(_) => {}
+    }
+
+    // shift_jisから変換する。
     encoding::all::WINDOWS_31J
-        .decode_to(&content, DecoderTrap::Strict, out)
-        .or_else(|_| UTF8Encoding.decode_to(&content, DecoderTrap::Strict, out))
+        .decode_to(&contents, DecoderTrap::Strict, out)
         .is_ok()
 }
