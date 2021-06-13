7c8a01190f4b6e812689640a2f64fa85e5068da2
vain0x <vainzerox@gmail.com>
2021-06-14 00:11:07 +0900

    refactor: 関数を分けるなど (completion)

--- hsp3-analyzer-mini/ham-core/src/assists/completion.rs
+++ hsp3-analyzer-mini/ham-core/src/assists/completion.rs
@@ -104,6 +104,79 @@ pub(crate) fn collect_symbols_as_completion_items(
     }
 }
 
+fn to_completion_symbol_kind(kind: HspSymbolKind) -> CompletionItemKind {
+    // textDocument/documentSymbol, workspace/symbol も参照
+    use CompletionItemKind as K;
+
+    match kind {
+        HspSymbolKind::Unresolved | HspSymbolKind::Unknown => K::Text,
+        HspSymbolKind::Label => K::Value,
+        HspSymbolKind::StaticVar => K::Variable,
+        HspSymbolKind::Const => K::Constant,
+        HspSymbolKind::Enum => K::EnumMember,
+        HspSymbolKind::Macro { ctype: false } => K::Value,
+        HspSymbolKind::Macro { ctype: true } => K::Function,
+        HspSymbolKind::DefFunc => K::Method,
+        HspSymbolKind::DefCFunc => K::Function,
+        HspSymbolKind::ModFunc => K::Method,
+        HspSymbolKind::ModCFunc => K::Function,
+        HspSymbolKind::Param(None) => K::Variable,
+        HspSymbolKind::Param(Some(param)) => match param.category() {
+            PParamCategory::ByValue => K::Value,
+            PParamCategory::ByRef => K::Property,
+            PParamCategory::Local => K::Variable,
+            PParamCategory::Auto => K::Text,
+        },
+        HspSymbolKind::Module => K::Module,
+        HspSymbolKind::Field => K::Field,
+        HspSymbolKind::LibFunc => K::Function,
+        HspSymbolKind::PluginCmd => K::Keyword,
+        HspSymbolKind::ComInterface => K::Interface,
+        HspSymbolKind::ComFunc => K::Method,
+    }
+}
+
+fn to_lsp_completion_item(symbol: &SymbolRc) -> CompletionItem {
+    let details = symbol.compute_details();
+    let detail = details.desc.map(|s| s.to_string());
+    let documentation = if details.docs.is_empty() {
+        None
+    } else {
+        Some(Documentation::String(details.docs.join("\r\n\r\n")))
+    };
+
+    let sort_text = {
+        let sort_prefix = match (&symbol.scope_opt, symbol.kind) {
+            (Some(Scope::Local(local)), _) => match (&local.module_opt, local.deffunc_opt) {
+                (Some(_), Some(_)) => 'a',
+                (Some(_), None) => 'b',
+                (None, None) => 'c',
+                (None, Some(_)) => 'd',
+            },
+            (_, HspSymbolKind::Module) => 'f',
+            (Some(Scope::Global), _) => 'e',
+            (None, _) => 'g',
+        };
+        Some(format!("{}{}", sort_prefix, symbol.name))
+    };
+
+    CompletionItem {
+        kind: Some(to_completion_symbol_kind(symbol.kind)),
+        label: symbol.name.to_string(),
+        detail,
+        documentation,
+        sort_text,
+        ..CompletionItem::default()
+    }
+}
+
+fn new_completion_list(items: Vec<CompletionItem>) -> CompletionList {
+    CompletionList {
+        is_incomplete: false,
+        items,
+    }
+}
+
 pub(crate) fn incomplete_completion_list() -> CompletionList {
     CompletionList {
         is_incomplete: true,
@@ -128,10 +201,7 @@ fn do_completion(
     if wa.in_preproc(doc, pos).unwrap_or(false) {
         wa.require_project_for_doc(doc)
             .collect_preproc_completion_items(&mut items);
-        return Some(CompletionList {
-            is_incomplete: false,
-            items,
-        });
+        return Some(new_completion_list(items));
     }
 
     let mut completion_items = vec![];
@@ -145,68 +215,7 @@ fn do_completion(
                     continue;
                 }
 
-                let details = symbol.compute_details();
-
-                // textDocument/documentSymbol, workspace/symbol も参照
-                use CompletionItemKind as K;
-
-                let kind = match symbol.kind {
-                    HspSymbolKind::Unresolved | HspSymbolKind::Unknown => K::Text,
-                    HspSymbolKind::Label => K::Value,
-                    HspSymbolKind::StaticVar => K::Variable,
-                    HspSymbolKind::Const => K::Constant,
-                    HspSymbolKind::Enum => K::EnumMember,
-                    HspSymbolKind::Macro { ctype: false } => K::Value,
-                    HspSymbolKind::Macro { ctype: true } => K::Function,
-                    HspSymbolKind::DefFunc => K::Method,
-                    HspSymbolKind::DefCFunc => K::Function,
-                    HspSymbolKind::ModFunc => K::Method,
-                    HspSymbolKind::ModCFunc => K::Function,
-                    HspSymbolKind::Param(None) => K::Variable,
-                    HspSymbolKind::Param(Some(param)) => match param.category() {
-                        PParamCategory::ByValue => K::Value,
-                        PParamCategory::ByRef => K::Property,
-                        PParamCategory::Local => K::Variable,
-                        PParamCategory::Auto => K::Text,
-                    },
-                    HspSymbolKind::Module => K::Module,
-                    HspSymbolKind::Field => K::Field,
-                    HspSymbolKind::LibFunc => K::Function,
-                    HspSymbolKind::PluginCmd => K::Keyword,
-                    HspSymbolKind::ComInterface => K::Interface,
-                    HspSymbolKind::ComFunc => K::Method,
-                };
-
-                // 候補の順番を制御するための文字。(スコープが狭いものを上に出す。)
-                let sort_prefix = match (&symbol.scope_opt, symbol.kind) {
-                    (Some(Scope::Local(local)), _) => {
-                        match (&local.module_opt, local.deffunc_opt) {
-                            (Some(_), Some(_)) => 'a',
-                            (Some(_), None) => 'b',
-                            (None, None) => 'c',
-                            (None, Some(_)) => 'd',
-                        }
-                    }
-                    (_, HspSymbolKind::Module) => 'f',
-                    (Some(Scope::Global), _) => 'e',
-                    (None, _) => 'g',
-                };
-
-                let detail = details.desc.map(|s| s.to_string());
-                let documentation = if details.docs.is_empty() {
-                    None
-                } else {
-                    Some(Documentation::String(details.docs.join("\r\n\r\n")))
-                };
-
-                items.push(CompletionItem {
-                    kind: Some(kind),
-                    label: symbol.name.to_string(),
-                    detail,
-                    documentation,
-                    sort_text: Some(format!("{}{}", sort_prefix, symbol.name)),
-                    ..CompletionItem::default()
-                });
+                items.push(to_lsp_completion_item(&symbol));
             }
         }
     }
@@ -232,10 +241,7 @@ fn do_completion(
         });
     }
 
-    Some(CompletionList {
-        is_incomplete: false,
-        items,
-    })
+    Some(new_completion_list(items))
 }
 
 #[derive(Serialize, Deserialize)]
