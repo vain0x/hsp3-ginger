6d73b1066ff24dd4b5b73e5ae023604f90715e68
vain0x <vainzerox@gmail.com>
2024-04-19 01:47:57 +0900

    feat(ham): rowan構文木への変換、文字列化

--- hsp3-analyzer-mini/ham-core/src/commands.rs
+++ hsp3-analyzer-mini/ham-core/src/commands.rs
@@ -1,21 +1,91 @@
 //! (LSPサーバー以外の) CLIコマンドの処理
 
+use rowan::Language;
+
 use crate::{
-    parse::{parse_root, PToken},
+    parse::{parse_root, PRoot, PToken, PVisitor},
     source::DocId,
     utils::{rc_str::RcStr, read_file::read_file},
 };
 use std::{
     hash::{Hash, Hasher},
-    io::{stdin, stdout, Read, Write},
+    io::{stdin, stdout, Read, Write as _},
     path::PathBuf,
     time::Duration,
 };
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum HspLang {}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum MySyntaxKind {
+    Token,
+    Stmt,
+}
+
+impl rowan::api::Language for HspLang {
+    type Kind = MySyntaxKind;
+
+    fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+        match raw.0 {
+            1 => MySyntaxKind::Token,
+            2 => MySyntaxKind::Stmt,
+            _ => unreachable!(),
+        }
+    }
+
+    fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+        let n = match kind {
+            MySyntaxKind::Token => 1,
+            MySyntaxKind::Stmt => 2,
+        };
+        rowan::SyntaxKind(n)
+    }
+}
+
+struct V<'a> {
+    builder: rowan::GreenNodeBuilder<'a>,
+}
+
+impl<'a> PVisitor for V<'a> {
+    fn on_token(&mut self, token: &PToken) {
+        let kind = HspLang::kind_to_raw(MySyntaxKind::Token);
+        for t in token
+            .leading
+            .iter()
+            .chain(std::iter::once(token.body.as_ref()))
+            .chain(token.trailing.iter())
+        {
+            self.builder.token(kind, &t.text);
+        }
+    }
+
+    fn on_stmt(&mut self, stmt: &crate::parse::PStmt) {
+        let kind = HspLang::kind_to_raw(MySyntaxKind::Stmt);
+        self.builder.start_node(kind);
+        self.on_stmt_default(stmt);
+        self.builder.finish_node()
+    }
+}
+
+mod to_rowan {
+    use super::*;
+
+    pub(super) fn green_node(root: &PRoot) -> rowan::GreenNode {
+        let mut v = V {
+            builder: rowan::GreenNodeBuilder::new(),
+        };
+        let kind = HspLang::kind_to_raw(MySyntaxKind::Stmt); // root
+        v.builder.start_node(kind);
+        v.on_root(root);
+        v.builder.finish_node();
+        v.builder.finish()
+    }
+}
+
 /// ファイルを構文解析して構文木を出力する (出力内容は仮)
 pub fn parse(files: Vec<String>) {
     assert!(!files.is_empty());
-    let multiple = files.len() != 0;
 
     for (file_index, filename) in files.into_iter().enumerate() {
         let text = if filename == "-" {
@@ -37,12 +107,11 @@ pub fn parse(files: Vec<String>) {
         let tokens = PToken::from_tokens(tokens.into());
         let root = parse_root(tokens);
 
+        let green = to_rowan::green_node(&root);
+        let node: rowan::SyntaxNode<HspLang> = rowan::SyntaxNode::new_root(green);
+
         let mut out = stdout().lock();
-        if multiple {
-            write!(out, "file: {filename}\n{root:?}\n").unwrap();
-        } else {
-            write!(out, "{root:?}\n").unwrap();
-        }
+        write!(out, "file: {filename}\n{node:#?}").unwrap();
     }
 }
 
