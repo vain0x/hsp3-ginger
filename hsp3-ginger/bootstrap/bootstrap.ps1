25945734decaae44a35aefea7f2ca475870c82ac
vain0x <vainzerox@gmail.com>
2025-09-13 17:18:23 +0900

    feat(ginger): HSP3.7用に bootstrap スクリプトを更新

--- hsp3-ginger/bootstrap/bootstrap.ps1
+++ hsp3-ginger/bootstrap/bootstrap.ps1
@@ -1,50 +1,56 @@
 # bootstrap.hsp を使って ginger (CLI) の実行ファイルを作成する。
 # 使い方: bootstrap/bootstrap "path/to/hsp"
 
-$hsp3Home = $args[0]
+$ErrorActionPreference = 'Stop'
 
-if (!$hsp3Home) {
-    $hsp3Home = $env:HSP3_HOME
+$hsp3Root = $args[0]
+if (!$hsp3Root) {
+    $hsp3Root = $env:HSP3_ROOT
+
+    if (!$hsp3Root) {
+        $hsp3Root = $env:HSP3_HOME # 後方互換性のため (HSP3_ROOT/HSP3_HOME で混乱していた)
+    }
 }
-
-if (!$hsp3Home) {
-    write-error '引数または環境変数 HSP3_HOME に HSP のインストールディレクトリを指定してください。'
+if (!$hsp3Root) {
+    write-error '引数または環境変数 HSP3_ROOT に HSP のインストールディレクトリを指定してください。'
     exit 1
 }
 
-$workDir = (get-item .).fullName
+$baseDir = (get-item .).fullName
 
-# 実行ファイル生成用のスクリプトをコンパイルする。
-& "$hsp3Home/hspcmp.exe" "--compath=$hsp3Home/common/" "$workDir/bootstrap/bootstrap.hsp"
-if (!$?) {
-    write-error 'bootstrap.hsp のコンパイルに失敗しました。'
-    exit 1
-}
-
-# AXファイルが生成されたことを確認する。
-$bootstrapAx = "$workDir/bootstrap/bootstrap.ax"
-if (!(test-path $bootstrapAx)) {
-    write-error "$bootstrapAx が見つかりません。"
-    exit 1
-}
-
-# ginger の実行ファイルを生成する。
 try {
-    cd $hsp3Home
+    chdir 'bootstrap'
 
-    & "$hsp3Home/hsp3cl.exe" $bootstrapAx "$workDir/src/ginger_main_cli.hsp"
+    # 実行ファイル生成用のスクリプトをコンパイルする。
+    & "$hsp3Root/hspcmp.exe" "--compath=$hsp3Root/common/" "$baseDir/bootstrap/bootstrap.hsp"
+    if (!$?) {
+        write-error 'bootstrap.hsp のコンパイルに失敗しました。'
+        exit 1
+    }
+
+    # AXファイルが生成されたことを確認する。
+    $bootstrapAx = './bootstrap.ax'
+    if (!(test-path $bootstrapAx)) {
+        write-error "$bootstrapAx が見つかりません。"
+        exit 1
+    }
+
+    # ginger の実行ファイルを生成する。
+    & "$hsp3Root/hsp3cl.exe" $bootstrapAx
     if (!$?) {
         write-error 'ginger の実行ファイル生成に失敗しました。'
         exit 1
     }
 
-    & "$hsp3Home/ginger.exe" build "$workDir/src/ginger_main_gui.hsp"
-    if (!$?) {
-        write-error 'ginger_gui の実行ファイル生成に失敗しました。'
-        exit 1
-    }
+    # & "$baseDir/src/ginger.exe" build --hsp $hsp3Root "$baseDir/src/ginger_main_gui.hsp"
+    # if (!$?) {
+    #     write-error 'ginger_gui の実行ファイル生成に失敗しました。'
+    #     exit 1
+    # }
 
-    cp "$hsp3Home/ginger.exe" "$workDir/bin/ginger.exe"
+    move-item -force "$baseDir/src/ginger.exe" "$baseDir/bin/ginger.exe"
+
+    echo 'OK'
 } finally {
-    cd $workDir
+    cd $baseDir
 }
